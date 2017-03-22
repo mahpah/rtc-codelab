3eebf00001ffc2ace4010facf8af2f37d1ea9f18
mahpah <ha.pham@digimed.vn>
2017-03-22 15:46:25 +0700

    same file connect

--- work/js/main.js
+++ work/js/main.js
@@ -1,2 +1,62 @@
-'use strict';
+let serverless = null
+let alice
+let bob
+let mirror = document.querySelector('#mirror')
+let screen = document.querySelector('#screen')
+let localStream
+let initted
 
+const init = () => {
+	navigator.mediaDevices.getUserMedia({
+		video: true,
+		audio: false,
+	}).then(stream => {
+		initted = true
+		mirror.srcObject = stream
+		localStream = stream
+	})
+}
+
+const call = () => {
+	if (!initted) {
+		return
+	}
+
+	alice = new RTCPeerConnection(serverless)
+
+	alice.onicecandidate = (event) => {
+		bob.addIceCandidate(new RTCIceCandidate(event.candidate))
+	}
+
+	bob = new RTCPeerConnection(serverless)
+	bob.onicecandidate = () => {
+		alice.addIceCandidate(new RTCIceCandidate(event.candidate))
+	}
+
+	bob.onaddstream = (event) => {
+		screen.srcObject = event.stream
+	}
+
+	alice.addStream(localStream)
+	alice.createOffer({
+		offerToReceiveVideo: 1,
+		offerToReceiveAudio: 1,
+	}).then((offerDesc) => {
+		alice.setLocalDescription(offerDesc)
+		return bob.setRemoteDescription(offerDesc)
+	}).then(() => {
+		return bob.createAnswer()
+	}).then(answerDesc => {
+		bob.setLocalDescription(answerDesc)
+		alice.setRemoteDescription(answerDesc)
+	})
+}
+
+const hang = () => {
+	alice.close()
+	bob.close()
+}
+
+document.querySelector('#btn-call').addEventListener('click', call)
+document.querySelector('#btn-hang').addEventListener('click', hang)
+init()
